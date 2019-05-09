0379b0e763a103a9cab9f1d488ce794e51ec1e02
exredz028 <49966522+exredz028@users.noreply.github.com>
2019-05-10 03:02:07 +0700

    Add files via upload

---
+++ Project by Bird(ล่าสุด)/connect.php
@@ -0,0 +1,9 @@
+<?php
+$username_connect = "root";
+$password_connect = "root";
+$hostname_connect = "localhost";
+database_connect = "Healty";
+
+$connection = mysql_connect($hostname_connect,$username_connect,$password_connect) or die ("Unable to connect to MySQL server."):
+mysql_query("SET NAMES UTF8"):
+?>
